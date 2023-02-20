b8b57d7be5d7249d69e07982348a5eaff5e4e336
Roman Chornyi <romchablack2@gmail.com>
2023-02-20 09:46:46 +0000

    Add game_2.py with check for draw

---
+++ game_2.py
@@ -0,0 +1,185 @@
+from IPython.display import clear_output
+import random
+
+board_default = [[7, 8, 9], [4, 5, 6], [1, 2, 3]]
+
+available_moves = list(range(1, 10))
+
+game_on = True
+
+
+# PRINT CURRENT BOARD
+def print_board(board):
+    print("The current board is: ")
+    for i in board:
+        print(i)
+    print("\n")
+
+
+# SELECT FIRST PLAYER
+def new_game():
+    is_digit = False
+    is_in_range = False
+    available_moves = [1, 2]
+    while is_digit == False or is_in_range == False:
+        player = input(
+            "Who start? Player 1 or Player 2 (enter 1 or 2): "
+            )
+        if player.isdigit():
+            is_digit = True
+            if int(player) in available_moves:
+                is_in_range = True
+                player = int(player)
+                return player
+            else:
+                print("Please enter value from range (1 or 2)")
+        else:
+            print("Please enter valid INT value")
+
+
+# SELECT NEXT PLAYER
+def who_is_next(player):
+    if player == 1:
+        player = 2
+    else:
+        player = 1
+    print(f"The next move is Player_{player}.")
+    return player
+
+
+# SELECT MOVE
+def select_position(moves):
+    is_digit = False
+    is_in_range = False
+    while is_digit != True or is_in_range != True:
+        # print_board()runtime
+        choice = input(f"Enter value from available range {moves}: ")
+        # print(choice)
+        if choice.isdigit():
+            is_digit = True
+            if int(choice) in moves:
+                is_in_range = True
+                moves.pop(moves.index(int(choice)))
+                return int(choice)
+            else:
+                print(f"Please enter value from range: {moves}")
+        else:
+            print(f"Please enter INT value from {moves}")
+
+
+# PUT ON BOARD
+def add_selected_option(player, move, board, board_default):
+    for i in range(len(board_default)):
+        for y in range(len(board_default[i])):
+            if board_default[i][y] == move:
+                if player == 1:
+                    board[i][y] = "X"
+                else:
+                    board[i][y] = "O"
+                return board
+
+
+# CHECK THE WINNER
+def check_winner(board):
+    winner = 0
+    # CHECK DIAGONAL
+    diagonal = tuple([board[y][y] for y in range(len(board))])
+    if diagonal == tuple(["X"]*3):
+        winner = 1
+        return winner
+    elif diagonal == tuple(["O"]*3):
+        winner = 2
+        return winner
+    # CHECK ROWS 
+    for i in range(len(board)):
+        if board[i] == tuple(["X"]*3):
+            winner = 1
+            return winner
+        elif board[i] == tuple(["O"]*3):
+            winner = 2
+            return winner
+    # ROTATE THE BOARD
+    board = list(zip(*board[::-1]))
+    # CHECK DIAGONAL
+    diagonal = tuple([board[y][y] for y in range(len(board))])
+    if diagonal == tuple(["X"]*3):
+        winner = 1
+        return winner
+    elif diagonal == tuple(["O"]*3):
+        winner = 2
+        return winner
+    # CHECK ROWS ROTATED
+    for i in range(len(board)):
+        if board[i] == tuple(["X"]*3):
+            winner = 1
+            return winner
+        elif board[i] == tuple(["O"]*3):
+            winner = 2
+            return winner
+    return winner
+
+
+# RANDOM SELECT PLAYER
+def choose_player():
+    player = random.randint(1, 2)
+    print(f"Player_{player} starts.")
+    return player
+
+
+# REPLAY
+def replay():
+    replay = input(
+        "Do you want to play another game? (Y or N): "
+        )
+    if replay in ["y", "Y", "yes", "Yes", "YES"]:
+        print("NEW GAME!!!")
+        return True
+    else:
+        print("Thank you for the game. Bye!")
+        return False
+
+
+# CHECK SPACE
+def check_space(moves):
+    if len(moves) > 0:
+        pass
+    else:
+        print("Draw")
+        replay()
+
+
+# PLAY THE GAME
+def play_the_game(game_on, board_default):
+    while game_on:
+        # newboard
+        board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        print_board(board)
+        # define full set of available moves
+        moves = list(range(1, 10))
+        print(moves)
+        # define first Player
+        player = choose_player()
+
+        winner = 0
+        while winner == 0:
+            clear_output()
+            if moves != []:
+                # select move
+                position = select_position(moves)
+                # add move to the board
+                add_selected_option(player, position, board, board_default)
+                # print current board
+                print_board(board)
+                winner = check_winner(board)
+                if winner == 0:
+                    player = who_is_next(player)
+                else:
+                    print(f"Player_{winner} won the game.")
+                    game_on = replay()
+                    break
+            else:
+                print("draw")
+                game_on = replay()
+                break
+
+play_the_game(game_on, board_default)
